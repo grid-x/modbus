6b1c832e5f2a7e890cfb0ff61ddfe15036982d65
andig <cpuidle@gmx.de>
2023-04-10 21:13:30 +0200

    wip

--- ascii_over_tcp_client.go
+++ ascii_over_tcp_client.go
@@ -34,13 +34,13 @@ type asciiTCPTransporter struct {
 	tcpTransporter
 }
 
-func (mb *asciiTCPTransporter) Send(aduRequest []byte) (aduResponse []byte, err error) {
+func (mb *asciiTCPTransporter) Send(aduRequest []byte) ([]byte, error) {
 	mb.mu.Lock()
 	defer mb.mu.Unlock()
 
 	// Make sure port is connected
-	if err = mb.connect(); err != nil {
-		return
+	if err := mb.connect(); err != nil {
+		return nil, err
 	}
 	// Start the timer to close when idle
 	mb.lastActivity = time.Now()
@@ -50,21 +50,23 @@ func (mb *asciiTCPTransporter) Send(aduRequest []byte) (aduResponse []byte, err 
 	if mb.Timeout > 0 {
 		timeout = mb.lastActivity.Add(mb.Timeout)
 	}
-	if err = mb.conn.SetDeadline(timeout); err != nil {
-		return
+	if err := mb.conn.SetDeadline(timeout); err != nil {
+		return nil, err
 	}
 
 	// Send the request
 	mb.logf("modbus: send %q\n", aduRequest)
-	if _, err = mb.conn.Write(aduRequest); err != nil {
-		return
+	if _, err := mb.conn.Write(aduRequest); err != nil {
+		return nil, err
 	}
+
 	// Get the response
-	var n, length int
+	var length int
 	var data [asciiMaxSize]byte
 	for {
-		if n, err = mb.conn.Read(data[length:]); err != nil {
-			return
+		n, err := mb.conn.Read(data[length:])
+		if err != nil {
+			return nil, err
 		}
 		length += n
 		if length >= asciiMaxSize || n == 0 {
@@ -77,7 +79,7 @@ func (mb *asciiTCPTransporter) Send(aduRequest []byte) (aduResponse []byte, err 
 			}
 		}
 	}
-	aduResponse = data[:length]
-	mb.logf("modbus: recv %q\n", aduResponse)
-	return
+
+	mb.logf("modbus: recv %q\n", data[:length])
+	return data[:length], nil
 }
